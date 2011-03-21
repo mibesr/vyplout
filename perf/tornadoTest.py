f3e566dd74f566a3dd4353b3896d866cd4247bad
fjsllr <fjsllr@gmail.com>
2011-03-21 14:17:23 +0800

    添加性能测试

---
+++ perf/tornadoTest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""web main"""
+
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.web import RequestHandler, Application, authenticated
+
+#from rockps.auth import AuthHandler
+
+
+class MainHandler(RequestHandler):
+    def get(self):
+        self.write("hello world")
+        
+settings = {
+}
+
+application = Application([(r"/", MainHandler), ], **settings)
+
+if __name__ == "__main__":
+    http_server = HTTPServer(application)
+    http_server.listen(8081)
+    IOLoop.instance().start()
